1fd5c57fc6cf4a655478d2cd9b30be2211b76942
Ewelina <ewecia.s@gmail.com>
2026-01-28 16:50:03 +0100

    feat(backend): init spring boot with postgres and liquidbase

---
+++ fitness-app/src/main/kotlin/org/fitnessapp/fitnessapp/FitnessAppApplication.kt
@@ -0,0 +1,11 @@
+package org.fitnessapp.fitnessapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FitnessAppApplication
+
+fun main(args: Array<String>) {
+    runApplication<FitnessAppApplication>(*args)
+}
---
+++ fitness-app/src/test/kotlin/org/fitnessapp/fitnessapp/FitnessAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.fitnessapp.fitnessapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FitnessAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
